9dc6e487d54a4f864ccf6dbe4410d532e7f7961b
KJSturgill <122930021+KJSturgill@users.noreply.github.com>
2023-08-04 13:43:00 -0400

    Update cipher.py
    after an unhinged amount of typos fixed, my cypher passed the test

--- cipher.py
+++ cipher.py
@@ -1,1 +1,13 @@
 # add your code here
+plaintext= input("Enter sentence to encrypt:").lower()
+encryptedtext = ""
+for i in range(len(plaintext)):
+        letter = plaintext[i]
+
+        if ord(letter) < 97 or ord(letter) > 122:
+            encryptedtext+=letter
+
+        else:
+            encryptedtext += chr((ord(letter) + 5-97) % 26 + 97)
+
+print("The encrypted sentence is: " + encryptedtext)  
